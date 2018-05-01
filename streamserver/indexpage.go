a98038de555f2d754819779f48223cf792934a33
Andy Ka_ban <ka_ban@rambler.ru>
2018-05-02 01:24:41 +0300

    renew html template

--- streamserver/indexpage.go
+++ streamserver/indexpage.go
@@ -10,10 +10,37 @@ var indexPage = `
 <html>
   <head>
     <title>Video Streaming Demonstration</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script>
+            function isMobileMac() {
+                if ( navigator.userAgent.match(/iPhone/i)
+                     || navigator.userAgent.match(/iPad/i)
+                     || navigator.userAgent.match(/iPod/i))
+                   {
+                     return true;
+                   } 
+                   else {
+                     return false;
+                   }
+            }
+			$(document).ready(function() {
+				// Active camera will refresh every 2 seconds
+				var TIMEOUT = 1000;
+                if (isMobileMac())
+                {
+				    var refreshInterval = setInterval(function() {
+					    $('img#camera').attr('src', '/stream');
+				    }, TIMEOUT);
+                }
+                else {
+                    $("img#camera").attr("src", "/stream");
+                }
+			});
+		</script>
   </head>
   <body>
     <h1>Video Streaming Demonstration</h1>
-    <img src="/stream.jpg">
+    <img src="" id="camera">
   </body>
 </html>
 `
